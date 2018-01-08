2b52056986753fd5bdf77e2e6eb29456785fda06
Patrick Goebel <pgoebel@stanford.edu>
2018-01-07 17:04:48 -0800

    First working model with 2 pedestrians

--- svl_pedsim/nodes/simulator.py
+++ svl_pedsim/nodes/simulator.py
@@ -8,23 +8,66 @@
 
 import rospy
 from geometry_msgs.msg import Pose, Point, Quaternion
-from tf.transformations import quaternion_from_euler, euler_from_quaternion
-from svl_pedsim.agent import Agent
+from gazebo_msgs.msg import ModelState, ModelStates
+from svl_pedsim.agent import Agent, Model
 
+class SFM():
+    def __init__(self, k0_obstacle=1.0, k1_obstacle=2.0, k0_social=1.5, k1_social=3.0, k0_goal=10, k1_goal=300.0, goal_tolerance=0.5, delta_t=0.1):
+        self.k0_obstacle    = k0_obstacle
+        self.k1_obstacle    = k1_obstacle
+        self.k0_social      = k0_social
+        self.k1_social      = k1_social
+        self.k0_goal        = k0_goal
+        self.k1_goal        = k1_goal
+        self.goal_tolerance = goal_tolerance
+        self.delta_t        = delta_t
 
 class Simulator():
     def __init__(self):
         
         rospy.init_node('svl_pedsim_simulator')
         
-        agent1 = Agent(x=2.0, y=7.6)
+        rate = rospy.get_param('~rate', 20)
+        r = rospy.Rate(rate)
+        
+        # Track the Gazebo model states
+        self.model_states = ModelStates()
+        
+        # Subscriber to monitor all model states
+        rospy.wait_for_message("gazebo/model_states", ModelStates)
+        rospy.Subscriber("gazebo/model_states", ModelStates, self.get_model_states)
+        
+        # Publisher for moving the people models
+        model_state_pub = rospy.Publisher("gazebo/set_model_state", ModelState, queue_size=5)
+        
+        # Intialize the social forces model
+        sfm = SFM(k0_obstacle=1.0, k1_obstacle=75.0, k0_social=1.5, k1_social=3.0, k0_goal=10.0, k1_goal=300.0, goal_tolerance=0.5, delta_t=1.0/rate)
+        
+        # Create the first pedestrian
+        agent1 = Agent(hack_index=1, model=Model.person_1_walking_lidar, x=4.29, y=8.52, theta=0.0)
         agent1.spawn()
         
-        while not rospy.is_shutdown():
-            rospy.loginfo("COMPUTE!")
-            agent1.compute_forces()
-            rospy.sleep(2.0)
+        # Create the second pedestrian
+        agent2 = Agent(hack_index=2, model=Model.person_2_walking_lidar, x=15.0, y=8.48, theta=3.14)
+        agent2.spawn()
         
+        # Set the goal coordinates for each person
+        agent1.set_goal(15.0, 8.48359, 0.0)
+        agent2.set_goal(4.29, 8.52, 0.0)
+        
+        while not rospy.is_shutdown():
+            force1 = agent1.compute_forces(sfm)
+            agent1.move(force1, sfm)
+            model_state_pub.publish(agent1.model_state)
+            
+            force2 = agent2.compute_forces(sfm)
+            agent2.move(force2, sfm)
+            model_state_pub.publish(agent2.model_state)
+            
+            r.sleep()
+            
+    def get_model_states(self, msg):
+        self.model_states = msg
         
 if __name__ == '__main__':
     try:
--- svl_pedsim/src/svl_pedsim/agent.py
+++ svl_pedsim/src/svl_pedsim/agent.py
@@ -8,20 +8,13 @@
 
 import rospy
 from gazebo_msgs.msg import ModelState, ModelStates
-from gazebo_msgs.srv import SpawnModel, SpawnModelRequest
 from sensor_msgs.msg import LaserScan
 from std_srvs.srv import Empty
-from geometry_msgs.msg import Pose, Point, Quaternion
+from geometry_msgs.msg import Pose, Point, Quaternion, Twist
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
-from gazebo_ros import gazebo_interface as gazebo
 import uuid
 import os
-from math import sin, cos, atan2, pi
-
-#rospy.wait_for_service('gazebo/pause_physics', Empty)
-#rospy.wait_for_service('gazebo/unpause_physics', Empty)
-
-physics = gazebo.ODEPhysics()
+from math import sin, cos, atan2, exp, sqrt, pow, log10
 
 pause_physics = rospy.ServiceProxy('gazebo/pause_physics', Empty)
 unpause_physics = rospy.ServiceProxy('gazebo/unpause_physics', Empty)
@@ -32,63 +25,130 @@ class Type:
     obstacle = 2
     
 class Model:
-    person_walking_lidar  = "person_walking_lidar"
-    person_walking_rgbd   = "person_walking_rgbd"
-    person_standing_lidar = "person_standing_lidar"
-    person_standing_rgbd  = "person_standing_rgbd"
+    person_1_walking_lidar  = "person_1_walking_lidar"
+    person_2_walking_lidar  = "person_2_walking_lidar"
+    person_3_walking_lidar  = "person_3_walking_lidar"
+    person_4_walking_lidar  = "person_4_walking_lidar"
 
 class Agent(object):
-    def __init__(self, x=0, y=0, z=0, max_speed=1.34, fov=180, agent_type=Type.person, model=Model.person_walking_lidar):
+    def __init__(self, hack_index=1, x=0, y=0, z=0, theta=0, radius=0.5, mass=68.0, max_speed=1.34, fov=180, agent_type=Type.person, model=Model.person_1_walking_lidar):
         
-        self.id         = self.create_id()
-        self.agent_type = agent_type
-        self.model      = model
-        self.max_speed  = max_speed
-        self.fov        = fov
-        self.x          = x
-        self.y          = y
-        self.z          = z
+        self.hack_index  = hack_index
+        self.id          = self.create_id()
+        self.model_state = ModelState()
+        self.agent_type  = agent_type
+        self.model       = model
+        self.name        = self.model + "_" + str(self.id)
+        self.max_speed   = max_speed
+        self.fov         = fov
+        self.radius      = radius
+        self.mass        = mass
+        
+        self.x           = x
+        self.y           = y
+        self.z           = z
+        self.theta       = theta
+        self.force       = 0.0
+        self.pose        = Pose(position=Point(x,y,z))
+        self.velocity    = Twist()
+        self.last_update = None
+        
+        quaternion = quaternion_from_euler(0, 0, theta)
+        
+        self.pose.orientation.x = quaternion[0]
+        self.pose.orientation.y = quaternion[1]
+        self.pose.orientation.z = quaternion[2]
+        self.pose.orientation.w = quaternion[3]
+        
+        self.model_state.model_name = self.name
+        self.model_state.reference_frame = 'world'
+        self.model_state.pose = self.pose
         
     def spawn(self):
         spawn_command = "rosrun gazebo_ros spawn_model -database " + self.model \
-            + " -sdf -model " + self.model + "_" + str(self.id) + " -x " + str(self.x) + " -y " + str(self.y) + " -z " + str(self.z)
+            + " -sdf -model " + self.name + " -x " + str(self.x) + " -y " \
+            + str(self.y) + " -z " + str(self.z) + " -Y " + str(self.theta)
 
         pause_physics()
         os.system(spawn_command)
         unpause_physics()
         
+        # Hack to assign laser topic name as hard coded in SDF model file
+        topic_name = "/person" + "_" + str(self.hack_index) + "/scan"
         self.lidar_msg = LaserScan()
-        self.lidar_sub = rospy.Subscriber("/person/scan", LaserScan, self.lidar_callback)
+        self.lidar_sub = rospy.Subscriber(topic_name, LaserScan, self.lidar_callback)
         
     def set_goal(self, x, y, z=0):
         self.goal_x = x
         self.goal_y = y
         self.goal_z = z
         
+    def compute_forces(self, sfm):
+        # Goal force (attractive)
+        distance_to_goal = sqrt(pow(self.goal_x - self.x, 2) + pow(self.goal_y - self.y, 2))
+        theta_goal = atan2(self.goal_y - self.y, self.goal_x - self.x)
+        
+        # Constant goal force model
+        if distance_to_goal < sfm.goal_tolerance:
+            force_goal_x = 0.0
+            force_goal_y = 0.0
+        else:
+            force_goal_x = sfm.k0_goal * cos(theta_goal)
+            force_goal_y = sfm.k0_goal * sin(theta_goal)
+        
+        # Net obstacle force (repulsive)
+        force_obstacle_x = 0
+        force_obstacle_y = 0
+        
+        angle = self.lidar_msg.angle_min
+                
+        for distance in self.lidar_msg.ranges:
+            # Exponential obstacle force model
+            force_obstacle_x += exp(-sfm.k0_obstacle * (distance - self.radius)) * cos(angle + self.theta)
+            force_obstacle_y += exp(-sfm.k0_obstacle * (distance - self.radius)) * sin(angle + self.theta)
+            angle += self.lidar_msg.angle_increment
+            
+        # Net social force (repulsive)
+        # TO DO
+        
+        # Net force in the frame attached to the pedestrian
+        force_x = sfm.k1_goal * force_goal_x - sfm.k1_obstacle * force_obstacle_x
+        force_y = sfm.k1_goal * force_goal_y - sfm.k1_obstacle * force_obstacle_y
+        
+        return (force_x, force_y)
+        
+    def move(self, force, sfm):
+        force_x, force_y = force
+        
+        # Newton's Law
+        acceleration_x = force_x / self.mass
+        acceleration_y = force_y / self.mass
+
+        # Update velocity
+        self.velocity.linear.x += acceleration_x * sfm.delta_t
+        self.velocity.linear.y += acceleration_y * sfm.delta_t
+        
+        # TO DO: Rotate model to face direction of motion
+    
+        # Compute distance traveled this time step
+        self.x += 0.5 * acceleration_x * sfm.delta_t * sfm.delta_t
+        self.y += 0.5 * acceleration_y * sfm.delta_t * sfm.delta_t
+        
+        # Update pose of this person
+        self.pose.position = Point(self.x, self.y, self.z)
+        
+        quaternion = quaternion_from_euler(0, 0, self.theta)
+        
+        self.pose.orientation.x = quaternion[0]
+        self.pose.orientation.y = quaternion[1]
+        self.pose.orientation.z = quaternion[2]
+        self.pose.orientation.w = quaternion[3]
+        
+        # Pose
+        self.model_state.pose = self.pose
+
     def lidar_callback(self, msg):
         self.lidar_msg = msg
-        
-    def compute_forces(self):
-        sum_x = 0
-        sum_y = 0
-        
-        angle = self.lidar_msg.angle_max
-        
-        count = 0
-        
-        #print self.lidar_msg.ranges
-        
-        for distance in self.lidar_msg.ranges:
-            sum_x += distance * cos(angle)
-            sum_y += distance * sin(angle)
-            if count == 0:
-                direction = atan2(sum_y, sum_x)
-                print "FIRST DIRECTION", direction
-            angle -= self.lidar_msg.angle_increment
-            count = 1
-        
-        direction = atan2(sum_y, sum_x)
-        print "Direction", angle, direction
         
     def create_id(self):
         return uuid.uuid4()
